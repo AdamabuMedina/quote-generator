112e2afddc98d4fa3e7d75dcd01877beb30c6d68
AdamabuMedina <adamabujamila@gmail.com>
2020-12-11 13:53:05 +0300

    верстка HTML проекта

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("testing")
+// Get Quote From API
+async function getQuote() {
+   const apiUrl = "https://api.quran.sutanlab.id/surah/2/255/?method=getQuote&lang=en&format=json";
+   try {
+      const response = await fetch(apiUrl)
+      const data = await response.json()
+      console.log(data)
+   } catch (error) {
+      getQuote()
+      console.log("упс, нет цитаты", error)
+   }
+}
+
+// On Load
+getQuote()
